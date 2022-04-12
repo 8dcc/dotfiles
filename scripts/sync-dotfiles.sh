9cf598d99c9e8bb35fa60076cae3ad70630d3173
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-12 14:37:57 +0200

    Cleaned output and added bold

--- scripts/sync-dotfiles.sh
+++ scripts/sync-dotfiles.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Script for doing checkout to the correct branches, and pulling all the submodules
+# Script for starting and getting the submodules (Commit saved in the main repo), 
+# doing checkout to the correct branches, and pulling all the submodules from origin.
 
-echo "Changing directory..."
+bold_t="$(tput bold)"        # \033[1m
+norm_t="$(tput sgr0)"        # \033[0m
+
+echo -e "${bold_t}[${norm_t}sync-dotfiles${bold_t}] Changing directory...${norm_t}"
 cd ..
-echo "Updating submodules..."
+echo -e "${bold_t}[${norm_t}sync-dotfiles${bold_t}] Updating submodules...${norm_t}\n"
 git submodule update --init --recursive
-echo "Checking out the main branch..."
+echo -e "\n${bold_t}[${norm_t}sync-dotfiles${bold_t}] Checking out the main branch...${norm_t}\n"
 git submodule foreach git checkout main
-echo "Pulling remote repositories..."
+echo -e "\n${bold_t}[${norm_t}sync-dotfiles${bold_t}] Pulling remote repositories...${norm_t}\n"
 git submodule foreach git pull origin main
-echo "All done!"
+echo -e "\n${bold_t}[${norm_t}sync-dotfiles${bold_t}] All done!${norm_t}\n"
