300af2a39c83c9fdb9538a316b540f00bbac1bc3
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-12 15:07:09 +0200

    Now checks if submodules and git module exist Added function for printing error (if submodule is not found) and for suggesting using the sync-dofiles script. Also added error message if git is not intalled.

--- scripts/python/pull-dotfiles.py
+++ scripts/python/pull-dotfiles.py
@@ -1,6 +1,12 @@
 # Python script for pulling each submodule
 
-import git
+try:
+    import git
+except Exception:
+    print("You need git to use this script! Make sure you run:")
+    print("  python3 -m pip install -r requirements.txt")
+    exit(1)
+
 try:
     from colorama import Fore, Style
     colorama_found = True
@@ -30,12 +36,33 @@ def cprint_pulled(repo_color, repo_name, commit_number):
     else:
         print("[%s] Pulled %s commits from origin/main..." % (repo_name, commit_number))
 
+def cprint_error(error_text):
+    if colorama_found:
+        print("%s%s[%s%sError%s%s] %s%s" % 
+                (Style.RESET_ALL, Fore.WHITE, Fore.RED, Style.BRIGHT, Style.RESET_ALL, Fore.WHITE,
+                    error_text, Style.RESET_ALL))
+    else:
+        print("[Error] %s" % (error_text))
+
+def cprint_error_sugestion():
+    if colorama_found:
+        print("%s%s[%s%sInfo%s%s] You might want to use %s--recurse-submodules%s when cloning, or run %sscripts/sync-dotfiles.sh%s to easily pull all of them.%s" % 
+                (Style.RESET_ALL, Fore.WHITE, Fore.BLUE, Style.BRIGHT, Style.RESET_ALL, Fore.WHITE,
+                    Style.BRIGHT, Style.NORMAL, Style.BRIGHT, Style.NORMAL, Style.RESET_ALL))
+    else:
+        print("[Info] You might want to use --recurse-submodules when cloning, or run scripts/sync-dotfiles.sh to easily pull all of them.")
+
 # -----------------------------------------------------------------------
 # Functions for each repo
 
 def pull_repo_linux():
     repo_path = "%s/linux" % main_repo_path
-    repo = git.Repo(repo_path)
+    try:
+        repo = git.Repo(repo_path)
+    except Exception:
+        cprint_error("I can't find a repo in %s..." % (repo_path))
+        cprint_error_sugestion()
+        exit(1)
     repo_origin = repo.remotes.origin
     cprint_pulling(Fore.BLUE, "Linux", "r4v10l1/arch-dotfiles")
     repo_origin.fetch()
@@ -45,7 +72,12 @@ def pull_repo_linux():
 
 def pull_repo_windows():
     repo_path = "%s/windows" % main_repo_path
-    repo = git.Repo(repo_path)
+    try:
+        repo = git.Repo(repo_path)
+    except Exception:
+        cprint_error("I can't find a repo in %s..." % (repo_path))
+        cprint_error_sugestion()
+        exit(1)
     repo_origin = repo.remotes.origin
     cprint_pulling(Fore.CYAN, "Windows", "r4v10l1/cmder-dotfiles")
     repo_origin.fetch()
@@ -55,7 +87,12 @@ def pull_repo_windows():
 
 def pull_repo_browser():
     repo_path = "%s/browser" % main_repo_path
-    repo = git.Repo(repo_path)
+    try:
+        repo = git.Repo(repo_path)
+    except Exception:
+        cprint_error("I can't find a repo in %s..." % (repo_path))
+        cprint_error_sugestion()
+        exit(1)
     repo_origin = repo.remotes.origin
     cprint_pulling(Fore.YELLOW, "Browser", "r4v10l1/browser-homepage")
     repo_origin.fetch()
@@ -65,7 +102,12 @@ def pull_repo_browser():
 
 def pull_repo_vim():
     repo_path = "%s/vim" % main_repo_path
-    repo = git.Repo(repo_path)
+    try:
+        repo = git.Repo(repo_path)
+    except Exception:
+        cprint_error("I can't find a repo in %s..." % (repo_path))
+        cprint_error_sugestion()
+        exit(1)
     repo_origin = repo.remotes.origin
     cprint_pulling(Fore.GREEN, "Vim", "r4v10l1/vim-dotfiles")
     repo_origin.fetch()
