c2fc5982ceea3b7230c05b07ca4229ed90cc37a2
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-09-06 18:15:28 +0200

    Add script for committing each submodule

---
+++ scripts/commit_submodules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Commits each submodule. stdout is disabled for ignoring errors,
+# keep that in mind.
+
+cd ..
+
+git add browser
+git commit -m "Update browser repo" 1>/dev/null
+git add linux 1>/dev/null
+git commit -m "Update linux repo" 1>/dev/null
+git add vim 1>/dev/null
+git commit -m "Update vim repo" 1>/dev/null
+git add windows 1>/dev/null
+git commit -m "Update windows repo" 1>/dev/null
