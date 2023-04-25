0b1f2f49756e8fb54e306e44c0ab617f82483329
8dcc <8dcc.git@gmail.com>
2023-04-25 20:41:22 +0200

    Add emacs submodule to scripts

--- scripts/commit-submodules.sh
+++ scripts/commit-submodules.sh
@@ -11,5 +11,7 @@ git add linux 1>/dev/null
 git commit -m "Update linux repo" 1>/dev/null
 git add vim 1>/dev/null
 git commit -m "Update vim repo" 1>/dev/null
+git add emacs 1>/dev/null
+git commit -m "Update emacs repo" 1>/dev/null
 git add windows 1>/dev/null
 git commit -m "Update windows repo" 1>/dev/null
--- scripts/python/pull-dotfiles.py
+++ scripts/python/pull-dotfiles.py
@@ -138,6 +138,24 @@ def pull_repo_vim():
     repo_origin.pull()
     cprint_pulled(Fore.GREEN, "Vim", commits_to_pull)
 
+def pull_repo_emacs():
+    repo_path = "%s/emacs" % main_repo_path
+    try:
+        repo = git.Repo(repo_path)
+    except Exception:
+        cprint_error("I can't find a repo in %s" % (repo_path))
+        cprint_error_suggestion()
+        exit(1)
+    if repo.head.is_detached:
+        cprint_detached(Fore.MAGENTA, "Emacs")
+        repo.git.checkout("main")
+    repo_origin = repo.remotes.origin
+    cprint_pulling(Fore.MAGENTA, "Emacs", "8dcc/emacs-dotfiles")
+    repo_origin.fetch()
+    commits_to_pull = repo.git.rev_list("--count", "HEAD..origin/main")
+    repo_origin.pull()
+    cprint_pulled(Fore.MAGENTA, "Emacs", commits_to_pull)
+
 # -----------------------------------------------------------------------
 # Pull each repo
 
@@ -146,5 +164,6 @@ def main():
     pull_repo_windows()
     pull_repo_browser()
     pull_repo_vim()
+    pull_repo_emacs()
 
 main()
