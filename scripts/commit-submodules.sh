d16a1c8344929dc766489ee5cc4930e6cd91db12
8dcc <8dcc.git@gmail.com>
2023-10-13 22:58:49 +0200

    Use function for commit-submodules.sh

--- scripts/commit-submodules.sh
+++ scripts/commit-submodules.sh
@@ -5,13 +5,13 @@
 
 cd ..
 
-git add browser
-git commit -m "Update browser repo" 1>/dev/null
-git add linux 1>/dev/null
-git commit -m "Update linux repo" 1>/dev/null
-git add vim 1>/dev/null
-git commit -m "Update vim repo" 1>/dev/null
-git add emacs 1>/dev/null
-git commit -m "Update emacs repo" 1>/dev/null
-git add windows 1>/dev/null
-git commit -m "Update windows repo" 1>/dev/null
+commit() {
+    git add $1 1>/dev/null
+    git commit -m "Update $1 repo" 1>/dev/null
+}
+
+commit "browser"
+commit "linux"
+commit "vim"
+commit "emacs"
+commit "windows"
